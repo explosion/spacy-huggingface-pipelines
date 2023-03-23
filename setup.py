84c18e3eff6e96d98079d2177dcc56192ad506a3
Adriane Boyd <adrianeboyd@gmail.com>
2023-03-23 11:11:25 +0100

    Fix package name

--- setup.py
+++ setup.py
@@ -3,4 +3,4 @@
 if __name__ == "__main__":
     from setuptools import setup, find_packages
 
-    setup(name="spacy_transformers_pipelines", packages=find_packages())
+    setup(name="spacy_transformers_pipeline", packages=find_packages())
