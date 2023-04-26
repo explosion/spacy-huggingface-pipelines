c33ed843b6021c1f6c772817e5f82e25a50ccb41
Adriane Boyd <adrianeboyd@gmail.com>
2023-04-26 10:31:32 +0200

    Improve error handling and warnings
    These components back off from batch to single processing in order to
    handle problematic docs that lead to errors for the transformers
    `Pipeline` processing (known issues are with "empty" docs like `""` or
    `" "` but there could potentially be other issues depending on the
    model).
    
    The warnings are a bit noisy, but I'd rather not crash on or skip entire
    batches containing problematic texts, and if there is no indication,
    then it's not clear to users why there can be huge speed differences
    between individual batches.

--- spacy_huggingface_pipelines/text_classification.py
+++ spacy_huggingface_pipelines/text_classification.py
@@ -79,26 +79,24 @@ class HfTextPipe(Pipe):
                 yield doc
 
     def _get_annotations(self, docs: List[Doc], batch_size) -> List[List[dict]]:
-        # TODO: warn when truncating? (I'm not sure you can detect this
-        # easily through the current pipeline API)
-        try:
-            with warnings.catch_warnings():
-                warnings.simplefilter("ignore", category=UserWarning)
+        if len(docs) > 1:
+            try:
                 return self.hf_pipeline(
                     [doc.text for doc in docs], batch_size=batch_size, top_k=None
                 )
-        except Exception:
-            # TODO: better UX
-            pass
+            except Exception:
+                warnings.warn(
+                    "Unable to process texts as batch, backing off to processing texts individually"
+                )
         outputs = []
         for doc in docs:
             try:
-                with warnings.catch_warnings():
-                    warnings.simplefilter("ignore", category=UserWarning)
-                    outputs.append(self.hf_pipeline(doc.text, top_k=None))
+                outputs.append(self.hf_pipeline(doc.text, top_k=None))
             except Exception:
-                # TODO: better UX
-                warnings.warn(f"Unable to process, skipping doc {repr(doc)}")
+                text_excerpt = doc.text if len(doc.text) < 100 else doc.text[:100] + "..."
+                warnings.warn(
+                    f"Unable to process, skipping annotation for doc '{text_excerpt}'"
+                )
                 outputs.append([])
         return outputs
 
